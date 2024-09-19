b8f5fecdbe7a8fd45de241262ac4037b79d956fc
typevalentine <koilui7112@gmail.com>
2024-09-19 13:36:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void Main (String[] args);
+    {
+        double springCost = 458.75;
+        double summerCost = 385.93;
+        double fallCost = 513.42;
+        double winterCost = 572.74;
+        
+
+    }
+}
