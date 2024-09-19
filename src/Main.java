e91484100c07a1cb706ed761e7009514ca8bc608
typevalentine <koilui7112@gmail.com>
2024-09-19 14:12:26 -0400

    Completed Task

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
-public class Main {
-    public static void Main (String[] args);
+public class Main
+{
+    public static void main(String[] args)
     {
         double springCost = 458.75;
         double summerCost = 385.93;
         double fallCost = 513.42;
         double winterCost = 572.74;
-        
+        double totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Spring cost: $" + springCost);
+        System.out.println("Summer cost: $" + summerCost);
+        System.out.println("Fall cost: $" + fallCost);
+        System.out.println("Winter cost: $" + winterCost);
+        System.out.println("Total cost is : $" + totalCost);
 
     }
 }
